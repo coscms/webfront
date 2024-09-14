00b3cc59aa1819f9f3f928caf30686f70e5859eb
Hank Shen <swh@admpub.com>
2024-09-14 23:33:47 +0800

    update

--- library/module/module_none.go
+++ library/module/module_none.go
@@ -4,15 +4,16 @@
 package module
 
 import (
+	bindataBackend "github.com/coscms/webcore/library/bindata"
 	"github.com/coscms/webcore/library/module"
 	"github.com/coscms/webfront/initialize/frontend"
 	"github.com/coscms/webfront/library/bindata"
 )
 
 func SetFrontendTemplate(key string, templatePath string) {
-	module.SetTemplate(frontend.TmplPathFixers.PathAliases, key, templatePath)
+	module.SetTemplate(bindataBackend.PathAliases, frontend.TmplPathFixers.PathAliases, key, templatePath)
 }
 
 func SetFrontendAssets(assetsPath string) {
-	module.SetAssets(bindata.StaticOptions, assetsPath)
+	module.SetAssets(bindataBackend.StaticOptions, bindata.StaticOptions, assetsPath)
 }
