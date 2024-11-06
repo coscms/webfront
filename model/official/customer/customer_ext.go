f6276625139edc3698dd8beba56702d426275f90
Hank Shen <swh@admpub.com>
2024-11-06 14:59:38 +0800

    update

--- model/official/customer/customer_ext.go
+++ model/official/customer/customer_ext.go
@@ -7,14 +7,14 @@ import (
 	modelLevel "github.com/coscms/webfront/model/official/level"
 )
 
-type CustomerSimple struct {
+type CustomerBase struct {
 	Id     uint64 `db:"id" bson:"id,omitempty" comment:"ID" json:"id" xml:"id"`
 	Name   string `db:"name" bson:"name" comment:"名称" json:"name" xml:"name"`
 	Gender string `db:"gender" bson:"gender" comment:"性别(male-男;female-女;secret-保密)" json:"gender" xml:"gender"`
 	Avatar string `db:"avatar" bson:"avatar" comment:"头像" json:"avatar" xml:"avatar"`
 }
 
-func (cs *CustomerSimple) Short_() string {
+func (c *CustomerBase) Short_() string {
 	return "official_customer"
 }
 
