2e736cdc3c30cfe2ea063a69bc6315187411f569
Hank Shen <swh@admpub.com>
2025-01-09 14:03:11 +0800

    update

--- initialize/init.go
+++ initialize/init.go
@@ -11,10 +11,10 @@ import (
 
 var nav = &navigate.List{}
 
-var Project = navigate.NewProject(echo.T(`内容管理`), `webx`, `/official/customer/index`, nav)
+var Project = navigate.NewProject(echo.T(`#short#内容管理`), `webx`, `/official/customer/index`, nav)
 
 func init() {
 	bootconfig.OfficialHomepage = `https://www.coscms.com`
-	httpserver.Backend.Navigate.Projects.Get(`nging`).Name = echo.T(`其它功能`)
+	httpserver.Backend.Navigate.Projects.Get(`nging`).Name = echo.T(`#short#其它功能`)
 	httpserver.Backend.Navigate.AddProject(1, Project)
 }
