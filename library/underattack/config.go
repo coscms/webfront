be0a679d42ff4b2fbecdaa45110ac65f16c0d323
Hank Shen <swh@admpub.com>
2025-02-28 18:46:13 +0800

    update

--- library/underattack/config.go
+++ library/underattack/config.go
@@ -1,17 +1,28 @@
 package underattack
 
 import (
+	"strings"
 	"sync"
 
 	"github.com/coscms/webcore/library/ipfilter"
 	"github.com/webx-top/echo"
 )
 
+func NewConfig() *Config {
+	return &Config{}
+}
+
 type Config struct {
 	On          bool
 	IPWhitelist string
 	filter      *ipfilter.IPFilter
 	sg          sync.Once
+}
+
+func (c *Config) FromStore(r echo.H) *Config {
+	c.On = r.Bool(`On`)
+	c.IPWhitelist = strings.TrimSpace(r.String(`IPWhitelist`))
+	return c
 }
 
 func (c *Config) IsAllowed(ctx echo.Context) bool {
