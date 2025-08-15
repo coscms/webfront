b91e6af09a79ceb30619bca00525dfc9d47717ac
Hank Shen <swh@admpub.com>
2025-08-15 12:55:47 +0800

    update

---
+++ library/sitemap/handler.go
@@ -0,0 +1,51 @@
+package sitemap
+
+import (
+	"path/filepath"
+	"strings"
+
+	"github.com/coscms/webcore/library/config"
+	"github.com/webx-top/com"
+	"github.com/webx-top/echo"
+)
+
+func handleFile(c echo.Context, sitemapDir string, fileName string) error {
+	lang := c.Lang().Normalize()
+	root := sitemapDir + echo.FilePathSeparator
+	file := root + lang + echo.FilePathSeparator + fileName
+	err := c.File(file)
+	if err == nil || err != echo.ErrNotFound {
+		return err
+	}
+	if lang != config.FromFile().Language.Default {
+		lang = config.FromFile().Language.Default
+		file = root + lang + echo.FilePathSeparator + fileName
+		err = c.File(file)
+	}
+	return err
+}
+
+func handleStatic(c echo.Context, sitemapDir string) error {
+	lang := c.Lang().Normalize()
+	root := sitemapDir + echo.FilePathSeparator + lang + echo.FilePathSeparator + `sitemaps`
+	var err error
+	root, err = filepath.Abs(root)
+	if err != nil {
+		return err
+	}
+	name := filepath.Join(root, c.Param("*"))
+	if !strings.HasPrefix(name, root) {
+		return echo.ErrNotFound
+	}
+	err = c.File(name)
+	if err == nil || err != echo.ErrNotFound || com.IsDir(root) {
+		return err
+	}
+	langDefault := config.FromFile().Language.Default
+	if lang != langDefault {
+		root = strings.TrimSuffix(root, lang+echo.FilePathSeparator+`sitemaps`) + langDefault + echo.FilePathSeparator + `sitemaps`
+		name = filepath.Join(root, c.Param("*"))
+		err = c.File(name)
+	}
+	return err
+}
--- library/sitemap/registry.go
+++ library/sitemap/registry.go
@@ -2,7 +2,6 @@ package sitemap
 
 import (
 	"path/filepath"
-	"strings"
 
 	"github.com/admpub/sitemap-generator/smg"
 	"github.com/webx-top/echo"
@@ -21,29 +20,12 @@ type Sitemap struct {
 func RegisterRoute(r echo.RouteRegister) {
 	sitemapDir := filepath.Join(echo.Wd(), `public`, `sitemap`)
 	r.Get(`/sitemap.xml`, func(c echo.Context) error {
-		lang := c.Lang().Normalize()
-		return c.File(sitemapDir + echo.FilePathSeparator + lang + echo.FilePathSeparator + `sitemap.xml`)
+		return handleFile(c, sitemapDir, `sitemap.xml`)
 	})
 	r.Get(`/sitemap_index.xml`, func(c echo.Context) error {
-		lang := c.Lang().Normalize()
-		return c.File(sitemapDir + echo.FilePathSeparator + lang + echo.FilePathSeparator + `sitemap_index.xml`)
+		return handleFile(c, sitemapDir, `sitemap_index.xml`)
 	})
 	r.Get("/sitemaps/*", func(c echo.Context) error {
-		return static(c, sitemapDir)
+		return handleStatic(c, sitemapDir)
 	})
-}
-
-func static(c echo.Context, sitemapDir string) error {
-	lang := c.Lang().Normalize()
-	root := sitemapDir + echo.FilePathSeparator + lang + echo.FilePathSeparator + `sitemaps`
-	var err error
-	root, err = filepath.Abs(root)
-	if err != nil {
-		return err
-	}
-	name := filepath.Join(root, c.Param("*"))
-	if !strings.HasPrefix(name, root) {
-		return echo.ErrNotFound
-	}
-	return c.File(name)
 }
