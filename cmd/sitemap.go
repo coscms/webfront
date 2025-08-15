8662c773da958b5b4f312c73192ef51528cf780d
Hank Shen <swh@admpub.com>
2025-08-15 19:21:56 +0800

    update

--- cmd/sitemap.go
+++ cmd/sitemap.go
@@ -1,10 +1,12 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
 	"net/url"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/spf13/cobra"
 	"github.com/webx-top/com"
@@ -22,16 +24,17 @@ import (
 var sitemapCmd = &cobra.Command{
 	Use:   "sitemap",
 	Short: "sitemap generate",
-	Long: `Usage ./webx sitemap <rootURL> [languageCode]
+	Long: `Usage ./webx sitemap <rootURL> [languageCode...]
 删除所有: ./webx sitemap --mode=clear
-删除指定域名或者指定语言: ./webx sitemap --mode=clear <rootURL> [languageCode]
-删除所有域名下指定语言: ./webx sitemap --mode=clear all [languageCode]`,
+删除指定域名或者指定语言: ./webx sitemap --mode=clear <rootURL> en,zh-CN
+删除所有域名下指定语言: ./webx sitemap --mode=clear all en,zh-CN`,
 	RunE: sitemapRunE,
 }
 
 var sitemapCfg = struct {
 	Mode     string // full(全量生成) / incr(增量生成)
-	AllChild bool   // 是否同时生成所有子页面中的网址
+	Group    string
+	AllChild bool // 是否同时生成所有子页面中的网址
 }{
 	Mode:     `incr`,
 	AllChild: true,
@@ -48,13 +51,13 @@ func normalizeLangCode(s *string) bool {
 	return true
 }
 
-func validateLangCode(langs []string) error {
+func validateLangCode(langCodes []string) error {
 	validLangs := make([]string, len(config.FromFile().Language.AllList))
 	for idx, lng := range config.FromFile().Language.AllList {
 		validLangs[idx] = echo.NewLangCode(lng).Normalize()
 	}
 	var incorrectLang []string
-	for _, _lang := range langs {
+	for _, _lang := range langCodes {
 		if !com.InSlice(_lang, validLangs) {
 			incorrectLang = append(incorrectLang, _lang)
 		}
@@ -65,7 +68,7 @@ func validateLangCode(langs []string) error {
 	return nil
 }
 
-func clearAllSubdirLanguages(langs []string) error {
+func clearAllSubdirLanguages(langCodes []string) error {
 	root := filepath.Join(echo.Wd(), `public`, `sitemap`)
 	dirs, err := os.ReadDir(root)
 	if err != nil {
@@ -80,28 +83,28 @@ func clearAllSubdirLanguages(langs []string) error {
 	if len(subDirs) == 0 {
 		return err
 	}
-	for _, _lang := range langs {
+	for _, _lang := range langCodes {
 		sitemap.RemoveLanguage(_lang, subDirs...)
 	}
 	return err
 }
 
 func sitemapRunE(cmd *cobra.Command, args []string) error {
-	var rootURL, lang string
-	com.SliceExtract(args, &rootURL, &lang)
+	var rootURL, langCode string
+	com.SliceExtract(args, &rootURL, &langCode)
 	if sitemapCfg.Mode == `clear` {
 		switch rootURL {
 		case ``, `all`:
-			if len(lang) == 0 {
+			if len(langCode) == 0 {
 				sitemap.RemoveAll()
 				return nil
 			}
-			langs := param.Split(lang, `,`).Filter(normalizeLangCode).String()
-			err := validateLangCode(langs)
+			langCodes := param.Split(langCode, `,`).Filter(normalizeLangCode).String()
+			err := validateLangCode(langCodes)
 			if err != nil {
 				return err
 			}
-			err = clearAllSubdirLanguages(langs)
+			err = clearAllSubdirLanguages(langCodes)
 			fmt.Println(`removing sitemap is done`)
 			return err
 		}
@@ -122,32 +125,49 @@ func sitemapRunE(cmd *cobra.Command, args []string) error {
 	}
 	subDir := u.Hostname()
 	if sitemapCfg.Mode == `clear` {
-		if len(lang) == 0 {
+		if len(langCode) == 0 {
 			sitemap.RemoveAll(subDir)
 			fmt.Println(`removing sitemap is done`)
 			return nil
 		}
-		langs := param.Split(lang, `,`).Filter(normalizeLangCode).String()
-		err = validateLangCode(langs)
+		langCodes := param.Split(langCode, `,`).Filter(normalizeLangCode).String()
+		err = validateLangCode(langCodes)
 		if err != nil {
 			return err
 		}
-		for _, _lang := range langs {
+		for _, _lang := range langCodes {
 			sitemap.RemoveLanguage(_lang, subDir)
 		}
 		fmt.Println(`removing sitemap is done`)
 		return nil
 	}
 
+	var groupItems []*echo.KVx[sitemap.Sitemap, any]
+	if len(sitemapCfg.Group) > 0 {
+		groups := param.Split(sitemapCfg.Group, `,`).Filter().String()
+		for _, group := range groups {
+			item := sitemap.Registry.GetItem(group)
+			if item != nil {
+				groupItems = append(groupItems, item)
+			}
+		}
+	} else {
+		groupItems = sitemap.Registry.Slice()
+	}
+
+	if len(groupItems) == 0 {
+		return errors.New(`No group found`)
+	}
+
 	eCtx := defaults.NewMockContext()
 
 	frontend.TempInitRoute(u.Host)
 
-	var prepare func(langs []string) error
+	var prepare func(langCodes []string) error
 	if sitemapCfg.Mode == `incr` {
-		prepare = func(langs []string) error {
-			for _, v := range sitemap.Registry.Slice() {
-				for _, _lang := range langs {
+		prepare = func(langCodes []string) error {
+			for _, v := range groupItems {
+				for _, _lang := range langCodes {
 					b, err := filecache.ReadCache(`sitemap`, v.K+`_`+_lang)
 					if err != nil && !os.IsNotExist(err) {
 						return err
@@ -162,11 +182,11 @@ func sitemapRunE(cmd *cobra.Command, args []string) error {
 		}
 	}
 	if prepare == nil {
-		prepare = func(langs []string) error { return nil }
+		prepare = func(_ []string) error { return nil }
 	}
-	save := func(langs []string) {
-		for _, v := range sitemap.Registry.Slice() {
-			for _, _lang := range langs {
+	save := func(langCodes []string) {
+		for _, v := range groupItems {
+			for _, _lang := range langCodes {
 				lastID := eCtx.Internal().Uint64(_lang + `.` + v.K + `LastID`)
 				if lastID <= 0 {
 					continue
@@ -178,35 +198,42 @@ func sitemapRunE(cmd *cobra.Command, args []string) error {
 			}
 		}
 	}
-	var langs []string
-	if len(lang) == 0 {
-		langs = make([]string, len(config.FromFile().Language.AllList))
+	var langCodes []string
+	if len(langCode) == 0 {
+		langCodes = make([]string, len(config.FromFile().Language.AllList))
 		for index, lang := range config.FromFile().Language.AllList {
-			langs[index] = echo.NewLangCode(lang).Normalize()
+			langCodes[index] = echo.NewLangCode(lang).Normalize()
 		}
 	} else {
-		langs = param.Split(lang, `,`).Filter(normalizeLangCode).String()
-		err = validateLangCode(langs)
+		langCodes = param.Split(langCode, `,`).Filter(normalizeLangCode).String()
+		err = validateLangCode(langCodes)
 		if err != nil {
 			return err
 		}
 	}
-	if err = prepare(langs); err != nil {
+	if err = prepare(langCodes); err != nil {
 		return err
 	}
-	for _, _lang := range langs {
+	for _, _lang := range langCodes {
 		err = sitemap.GenerateIndex(eCtx, rootURL, _lang, sitemapCfg.AllChild, subDir)
 		if err != nil {
 			fmt.Println(err.Error())
 		}
 	}
 	fmt.Println(`sitemap generation is complete`)
-	save(langs)
+	save(langCodes)
 	return err
 }
 
 func init() {
 	cmd.Add(sitemapCmd)
 	sitemapCmd.Flags().StringVar(&sitemapCfg.Mode, `mode`, sitemapCfg.Mode, `模式。支持的值有: full(全量生成) / incr(增量生成) / clear(删除)`)
+
+	slic := sitemap.Registry.Slice()
+	groups := make([]string, len(slic))
+	for i, v := range slic {
+		groups[i] = v.K
+	}
+	sitemapCmd.Flags().StringVar(&sitemapCfg.Group, `group`, sitemapCfg.Group, `组。指定多个时用半角逗号“,”隔开, 支持的值有: `+strings.Join(groups, ` / `)+` 等`)
 	sitemapCmd.Flags().BoolVar(&sitemapCfg.AllChild, `allChild`, sitemapCfg.AllChild, `是否同时生成所有子页面中的网址`)
 }
