389c4f79e5401c1d72a7eacb77eaf75ecfa538fb
Hank Shen <swh@admpub.com>
2025-08-17 13:38:20 +0800

    update

--- model/official/navigate_ext.go
+++ model/official/navigate_ext.go
@@ -38,7 +38,7 @@ func queryArticleCategory(c context.Context) interface{} {
 				Id:       category.Id,
 				ParentId: category.ParentId,
 				Title:    category.Name,
-				Url:      c.URLFor(`/articles` + ext + `?categoryId=` + fmt.Sprint(category.Id)),
+				Url:      ctx.URLFor(`/articles` + ext + `?categoryId=` + fmt.Sprint(category.Id)),
 			},
 			Extra: echo.H{
 				`object`: category,
