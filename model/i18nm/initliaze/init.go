41496e5c9cf0f9ca35b82a282a94a13067e0f1ae
Hank Shen <swhbox@foxmail.com>
2025-11-10 19:00:38 +0800

    update

---
+++ model/i18nm/initliaze/init.go
@@ -0,0 +1,18 @@
+package initliaze
+
+import (
+	"github.com/admpub/log"
+	"github.com/coscms/webcore/library/config/startup"
+	"github.com/coscms/webfront/model/i18nm"
+	"github.com/webx-top/echo/defaults"
+)
+
+func init() {
+	startup.OnAfter(`web.installed`, func() {
+		ctx := defaults.NewMockContext()
+		err := i18nm.Initialize(ctx)
+		if err != nil {
+			log.Errorf(`[i18nm.Initialize] %v`, err)
+		}
+	})
+}
