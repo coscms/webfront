0d347c3cc506efdbad355dfce86b3756518934b3
Hank Shen <swhbox@foxmail.com>
2025-12-11 10:14:30 +0800

    update

--- model/i18nm/initialize/init.go
+++ model/i18nm/initialize/init.go
@@ -1,18 +1,30 @@
 package initialize
 
 import (
+	"github.com/admpub/events"
 	"github.com/admpub/log"
 	"github.com/coscms/webcore/library/config/startup"
 	"github.com/coscms/webfront/model/i18nm"
+	"github.com/webx-top/echo"
 	"github.com/webx-top/echo/defaults"
 )
 
 func init() {
-	startup.OnAfter(`web.installed`, func() {
-		ctx := defaults.NewMockContext()
-		err := i18nm.Initialize(ctx)
-		if err != nil {
-			log.Errorf(`[i18nm.Initialize] %v`, err)
-		}
+	//onWebInstalled()
+	echo.OnCallback(`nging.upgrade.db.after`, func(e events.Event) error {
+		initI18Resources()
+		return nil
 	})
+}
+
+func onWebInstalled() {
+	startup.OnAfter(`web.installed`, initI18Resources)
+}
+
+func initI18Resources() {
+	ctx := defaults.NewMockContext()
+	err := i18nm.Initialize(ctx)
+	if err != nil {
+		log.Errorf(`[i18nm.Initialize] %v`, err)
+	}
 }
