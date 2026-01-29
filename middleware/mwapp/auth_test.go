ce8e667fcb884f5fe0d5d09b1efd766c9260a0e9
Hank Shen <swhbox@foxmail.com>
2026-01-29 13:51:07 +0800

    update

---
+++ middleware/mwapp/auth_test.go
@@ -0,0 +1,31 @@
+package mwapp
+
+import (
+	"net/url"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/webx-top/echo"
+	"github.com/webx-top/echo/defaults"
+	"github.com/webx-top/echo/param"
+)
+
+func TestAuth(t *testing.T) {
+	cfg := DefaultAuthAppConfig
+	cfg.SetSecretGetter(func(ctx echo.Context, appID string) (string, error) {
+		return `secret`, nil
+	})
+	cfg.SetDefaults()
+	ctx := defaults.NewMockContext()
+	ctx.Request().Form().Set(cfg.FormAppIDKey, `test`)
+	nowTs := param.AsString(time.Now().Unix())
+	sign := cfg.SignMaker()(url.Values{
+		cfg.FormAppIDKey: []string{`test`},
+		cfg.FormTimeKey:  []string{nowTs},
+	}, `secret`)
+	ctx.Request().Form().Set(cfg.FormSignKey, sign)
+	ctx.Request().Form().Set(cfg.FormTimeKey, nowTs)
+	err := cfg.Verify(ctx)
+	assert.NoError(t, err)
+}
