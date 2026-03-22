d9e3f39b0c7aa7a284ec146e1e6fdc4c0ddb67b0
Hank Shen <swhbox@foxmail.com>
2026-03-22 17:38:01 +0800

    update

--- library/xkv/xkv_test.go
+++ library/xkv/xkv_test.go
@@ -6,23 +6,29 @@ import (
 
 	"github.com/coscms/webfront/library/cache"
 	"github.com/stretchr/testify/assert"
+	"github.com/webx-top/echo"
 )
 
 func TestCacheString(t *testing.T) {
-	testFn := func() (string, error) {
-		var value string
+	testFn := func() (echo.KVList, error) {
+		var value echo.KVList
 		err := cache.XFunc(context.Background(), `test`, &value, func() (err error) {
-			value = `pong`
+			value = echo.KVList{
+				echo.NewKV(`k`, `v`),
+			}
 			println(`~~~~~~~~~~~~~~~~~~~~~~~~~~~~~query`)
 			return
 		}, cache.TTL(DefaultTTL))
 		return value, err
 	}
+	expected := echo.KVList{
+		echo.NewKV(`k`, `v`),
+	}
 	value, err := testFn()
 	assert.NoError(t, err)
-	assert.Equal(t, `pong`, value)
+	assert.Equal(t, expected, value)
 
 	value, err = testFn()
 	assert.NoError(t, err)
-	assert.Equal(t, `pong`, value)
+	assert.Equal(t, expected, value)
 }
