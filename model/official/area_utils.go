bc79d01217a547691f81b0c08fa4cf42d70a60dd
Hank Shen <swhbox@foxmail.com>
2026-03-25 13:04:18 +0800

    update

--- model/official/area_utils.go
+++ model/official/area_utils.go
@@ -2,6 +2,7 @@ package official
 
 import (
 	"github.com/coscms/webfront/dbschema"
+	"github.com/webx-top/db"
 	"github.com/webx-top/echo"
 )
 
@@ -17,9 +18,22 @@ func QueryAreaNames(ctx echo.Context, areaID uint, areaM *Area) (areaNames []str
 	if err != nil {
 		return
 	}
-	areaNames = make([]string, len(parents))
-	for index, parent := range parents {
-		areaNames[index] = parent.Name
+	areaNames = make([]string, 0, len(parents)+1)
+	if len(parents) > 0 {
+		countryM := dbschema.NewOfficialCommonAreaCountry(ctx)
+		countryM.Get(func(r db.Result) db.Result {
+			return r.Select(`name`)
+		}, `abbr`, parents[0].CountryAbbr)
+		if len(countryM.Name) > 0 {
+			areaNames = append(areaNames, countryM.Name)
+		} else if parents[0].CountryAbbr == `CN` {
+			areaNames = append(areaNames, `中国`)
+		} else {
+			areaNames = append(areaNames, parents[0].CountryAbbr)
+		}
+	}
+	for _, parent := range parents {
+		areaNames = append(areaNames, parent.Name)
 	}
 	return
 }
