8ba4991c226f659271df37219c0a9bb8ce78d76c
Hank Shen <swhbox@foxmail.com>
2026-06-18 16:47:37 +0800

    update

--- library/oauthutils/provider.go
+++ library/oauthutils/provider.go
@@ -6,6 +6,7 @@ import (
 
 	// - oauth2 provider
 	"github.com/admpub/goth/providers/microsoftonline"
+	"github.com/coscms/webfront/library/oauth2client/providers/webx"
 )
 
 // RegisterProvider 注册Provider
@@ -17,6 +18,16 @@ func RegisterProvider(c *oauth2.Config) {
 		}
 		m := microsoftonline.New(account.Key, account.Secret, account.CallbackURL)
 		m.SetName(`microsoft`)
+		return m
+	})
+
+	oauth2.Register(`coscms`, func(account *oauth2.Account) goth.Provider {
+		hostURL := account.Extra.String(`hostURL`, `https://www.coscms.com`)
+		if len(account.CallbackURL) == 0 {
+			account.CallbackURL = oauth2.DefaultPath + "/callback/" + account.Name
+		}
+		m := webx.New(account.Key, account.Secret, account.CallbackURL, hostURL, `profile`)
+		m.SetName(`coscms`)
 		return m
 	})
 
